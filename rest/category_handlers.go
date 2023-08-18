caf1949dc920563adb277279c17a991bc0be3bb7
Benson Smith <bsmith@tainosystems.com>
2023-08-18 11:28:17 -0400

    New Architechture 4.8

--- rest/category_handlers.go
+++ rest/category_handlers.go
@@ -12,70 +12,6 @@ import (
 )
 
 // CreateCategory crée une nouvelle catégorie.
-// func CreateCategory(w http.ResponseWriter, r *http.Request) {
-// 	// Décode le JSON de la requête dans une instance de Category
-// 	var category models.Category
-// 	err := json.NewDecoder(r.Body).Decode(&category)
-// 	if err != nil {
-// 		utils.Logger(err)
-// 		http.Error(w, err.Error(), http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	services.InsertCategory(&category)
-
-// 	w.WriteHeader(http.StatusCreated)
-// 	json.NewEncoder(w).Encode(category)
-// }
-
-// GetCategoryByID récupère une catégorie par son ID.
-// func GetCategoryByID(w http.ResponseWriter, r *http.Request) {
-//     params := mux.Vars(r)
-//     id := params["id"]
-//     result, err := strconv.Atoi(id)
-//     if err != nil {
-//         utils.Logger(err)
-//         http.Error(w, "Invalid ID", http.StatusBadRequest)
-//         return
-//     }
-
-//     c := services.GetCategoryByID(result)
-//     if c.ID == 0 { // Remplacez cette condition par celle qui indique que la catégorie n'a pas été trouvée
-//         http.Error(w, "Category not found", http.StatusNotFound)
-//         return
-//     }
-
-//     w.WriteHeader(http.StatusOK)
-//     json.NewEncoder(w).Encode(c)
-// }
-
-// GetAllCategories récupère toutes les catégories.
-// func GetAllCategories(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-
-// 	category := services.GetAllCategories()
-// 	w.WriteHeader(http.StatusOK)
-// 	json.NewEncoder(w).Encode(category)
-
-// }
-
-// // UpdateCategory met à jour une catégorie par son ID.
-// func UpdateCategory(w http.ResponseWriter, r *http.Request) {
-// 	var updatedCategory models.Category
-// 	w.Header().Set("Content-Type", "application/json")
-// 	services.UpdateCategory(updatedCategory.ID, updatedCategory)
-// 	w.WriteHeader(http.StatusOK)
-// 	json.NewEncoder(w).Encode(updatedCategory)
-// }
-
-// DeleteCategory supprime une catégorie par son ID.
-// func DeleteCategory(w http.ResponseWriter, r *http.Request) {
-// 	var category models.Category
-// 	services.DeleteCategory(category.ID)
-// 	w.WriteHeader(http.StatusOK)
-// 	fmt.Fprintf(w, "Category has been deleted")
-// }
-
 func CreateCategory(w http.ResponseWriter, r *http.Request) {
 	var newCategory models.Category
 	err := json.NewDecoder(r.Body).Decode(&newCategory)
@@ -85,16 +21,12 @@ func CreateCategory(w http.ResponseWriter, r *http.Request) {
 	}
 
 	services.InsertCategory(&newCategory)
-	// if err != nil {
-	// 	utils.Logger(err)
-	// 	http.Error(w, "Error inserting category", http.StatusInternalServerError)
-	// 	return
-	// }
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newCategory)
 }
 
+// GetCategoryByID récupère une catégorie par son ID.
 func GetCategoryByID(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id := params["id"]
@@ -115,6 +47,7 @@ func GetCategoryByID(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(c)
 }
 
+// GetAllCategories récupère toutes les catégories.
 func GetAllCategories(w http.ResponseWriter, r *http.Request) {
 	categories := services.GetAllCategories()
 	if categories == nil {
@@ -126,6 +59,7 @@ func GetAllCategories(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(categories)
 }
 
+// // UpdateCategory met à jour une catégorie par son ID.
 func UpdateCategory(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id := params["id"]
@@ -153,6 +87,7 @@ func UpdateCategory(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(updatedCategory)
 }
 
+// DeleteCategory supprime une catégorie par son ID.
 func DeleteCategory(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id := params["id"]
